b97ade2918030fdf670672192e077f969867c476
Raul Barreto <raul_barreto99@live.com>
2026-02-03 02:19:25 -0300

    route of fetch pets in a city

--- src/http/controllers/pets/routes.ts
+++ src/http/controllers/pets/routes.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { registerPets } from "./register-pets";
+import { fetchPetsInACity } from "./fetch-pets-in-a-city";
 
 export async function petsRoutes(app: FastifyInstance) {
     app.post('/pets', registerPets)
-
+    app.get('/pets/:city', fetchPetsInACity)
 }
